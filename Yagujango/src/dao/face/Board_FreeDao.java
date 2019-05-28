f4b0a2eaddf64f6b0fe80842811a3a6809688c88
JinBeom <clantata1@gmail.com>
2019-05-28 14:27:31 +0900

    2019-05-28
    게시글 리스트,간단한 뷰 작성
    공지 뷰는 미작성

--- Yagujango/src/dao/face/Board_FreeDao.java
+++ Yagujango/src/dao/face/Board_FreeDao.java
@@ -1,5 +1,21 @@
 package dao.face;
 
+import java.util.List;
+
+import dto.Board_Free;
+import util.Paging;
+
+
+
 public interface Board_FreeDao {
 
+	public int selectCntAll(Paging paging);
+	
+	public List<Board_Free> selectAll(Paging paging);
+	
+	public List<Board_Free> selectAllwithNotice(Paging paging);
+
+	public Board_Free selectBoardByBoardno(Board_Free viewboard);
+
+	public void upHit(Board_Free viewboard);
 }
